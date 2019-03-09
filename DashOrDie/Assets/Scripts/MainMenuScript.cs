8557ae0d08010882e36a1b49809c5a6efe817eb9
WestonElliott <Westonini101@gmail.com>
2019-03-09 01:54:01 -0800

    Added crappy but functioning main menu scene with proper transitions.

---
+++ DashOrDie/Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
