86b0cee66f3a1ffbbeb5de8bba24addca0add246
WestonElliott <Westonini101@gmail.com>
2019-05-03 00:03:27 -0700

    - Added lava particle effects - Added FreezePlayer script for upwards exits - Working on Level3

---
+++ DashOrDie/Assets/Scripts/FreezePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezePlayer : MonoBehaviour
+{
+
+    private PlayerController PC;
+    private Rigidbody2D rb;
+
+    void Awake()
+    {
+        PC = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+    }
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            rb = PC.GetComponent<Rigidbody2D>();
+            rb.isKinematic = true;
+        }
+    }
+}
